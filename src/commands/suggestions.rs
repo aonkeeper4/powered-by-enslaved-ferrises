e51656aa07d1d56dfa1040759acd3b4fd29f31c8
Dylan Strudwick <dylan@DESKTOP-31S60DJ.router.local>
2022-12-29 22:58:51 +0000

    finished edit title menu but it isnt working. also formatting

--- src/commands/suggestions.rs
+++ src/commands/suggestions.rs
@@ -1,10 +1,11 @@
 use chrono::{DateTime, Utc};
 use rand::prelude::*;
+use serenity::builder::CreateMessage;
 use serenity::framework::standard::macros::command;
-use serenity::framework::standard::{Args, CommandResult, CommandError};
-use serenity::model::prelude::component::ButtonStyle;
-use serenity::model::prelude::*;
+use serenity::framework::standard::{Args, CommandResult};
 use serenity::model::application::interaction::InteractionResponseType;
+use serenity::model::prelude::component::{ButtonStyle, InputTextStyle};
+use serenity::model::prelude::*;
 use serenity::prelude::*;
 use serenity::utils::Color;
 use std::time::Duration;
@@ -37,12 +38,11 @@ pub async fn create(ctx: &Context, msg: &Message, _args: Args) -> CommandResult 
 
     let mut suggestion = Suggestion::new();
 
-    loop {
-        // TODO: possibly restructure?
-        let menu = channel_id.send_message(&ctx.http, |m| {
+    let menu = channel_id
+        .send_message(&ctx.http, |m| {
             m.embed(|e| {
-                e.title(format!("Create Suggestion: {}", suggestion.title))
-                    .description("With a description")
+                e.title(format!("Create Suggestion: {}", &suggestion.title))
+                    .description(&suggestion.desc)
                     .footer(|f| {
                         f.text(format!(
                             "Time created: {} | ID: {}",
@@ -76,58 +76,139 @@ pub async fn create(ctx: &Context, msg: &Message, _args: Args) -> CommandResult 
                     })
                 })
             })
-        }).await?;
+        })
+        .await?;
+
+    loop {
+        // TODO: possibly restructure?
 
         let Some(interaction) = menu.await_component_interaction(ctx).timeout(Duration::from_secs(60 * 3)).await else {
             menu.reply(&ctx, "Timed out").await?;
             return Ok(());
         };
-
         let action = &interaction.data.custom_id;
+
         match action.as_str() {
             "edit_title" => {
-                interaction.create_interaction_response(&ctx, |r| {
-                    r.kind(InteractionResponseType::UpdateMessage).interaction_response_data(|d| {
-                        d.embed(|e| e.title("Edit Title")
-                            .description("Please reply to this message with the new title of your suggestion (timeout 30 seconds)")
-                            .footer(|f| {
-                                f.text(format!(
-                                    "Editing Suggestion ID {}",
-                                    suggestion.id
-                                ))
+                println!("editing title");
+                interaction
+                    .create_interaction_response(&ctx, |r| {
+                        r.kind(InteractionResponseType::UpdateMessage)
+                            .interaction_response_data(|d| {
+                                d.embed(|e| {
+                                    e.title("Edit Title")
+                                        .description(
+                                            "Please input the new title of your suggestion",
+                                        )
+                                        .footer(|f| {
+                                            f.text(format!(
+                                                "Editing Suggestion ID {}",
+                                                suggestion.id
+                                            ))
+                                        })
+                                        .color(Color::DARK_TEAL)
+                                })
+                                .components(|c| {
+                                    c.create_action_row(|a| {
+                                        a.create_input_text(|i| {
+                                            i.label("New title:")
+                                                .custom_id("new_title")
+                                                .style(InputTextStyle::Short)
+                                        })
+                                    })
+                                })
                             })
-                            .color(Color::DARK_TEAL)
-                        )
                     })
-                }).await?;
+                    .await?;
 
-                if let Some(new_title) = &msg.author.await_reply(ctx).timeout(Duration::from_secs(30)).await {
-                    suggestion.title = new_title.content.trim().to_string();
-                    new_title.delete(&ctx.http).await?;
-
-                    // TODO: fix so this edts the message and also has continue button
-                    interaction.create_followup_message(&ctx, |d| {
-                        d.embed(|e| e.title("Success")
-                            .description("Suggestion title successfully edited!")
-                            .footer(|f| {
-                                f.text(format!(
-                                    "Editing Suggestion ID {}",
-                                    suggestion.id
-                                ))
-                            })
-                            .color(Color::DARK_TEAL)
-                        )
-                    }).await?;
-                } else {
+                let Some(interaction) = menu.await_component_interaction(ctx).timeout(Duration::from_secs(60 * 3)).await else {
                     menu.reply(&ctx, "Timed out").await?;
                     return Ok(());
-                }
-            },
+                };
+                let new_title = &interaction.data.values[0];
+                suggestion.title = new_title.trim().to_string();
+
+                interaction
+                    .create_interaction_response(&ctx, |r| {
+                        r.kind(InteractionResponseType::UpdateMessage)
+                            .interaction_response_data(|d| {
+                                d.embed(|e| {
+                                    e.title("Success")
+                                        .description("Suggestion title was updated successfully!")
+                                        .footer(|f| {
+                                            f.text(format!(
+                                                "Editing Suggestion ID {}",
+                                                suggestion.id
+                                            ))
+                                        })
+                                        .color(Color::DARK_TEAL)
+                                })
+                                .components(|c| {
+                                    c.create_action_row(|a| {
+                                        a.create_button(|b| {
+                                            b.label("Continue")
+                                                .style(ButtonStyle::Success)
+                                                .custom_id("continue")
+                                        })
+                                    })
+                                })
+                            })
+                    })
+                    .await?;
+            }
             "edit_desc" => todo!("edit desc"),
             "send" => todo!("send"),
             "cancel" => todo!("cancel"),
             _ => panic!("invalid action"),
         }
+
+        let Some(interaction) = menu.await_component_interaction(ctx).timeout(Duration::from_secs(60 * 3)).await else {
+            menu.reply(&ctx, "Timed out").await?;
+            return Ok(());
+        };
+        interaction
+            .create_interaction_response(&ctx, |r| {
+                r.kind(InteractionResponseType::UpdateMessage)
+                    .interaction_response_data(|d| {
+                        d.embed(|e| {
+                            e.title(format!("Create Suggestion: {}", &suggestion.title))
+                                .description(&suggestion.desc)
+                                .footer(|f| {
+                                    f.text(format!(
+                                        "Time created: {} | ID: {}",
+                                        suggestion.time_created.format("%d/%m/%Y @%H:%M:%S UTC"),
+                                        suggestion.id
+                                    ))
+                                })
+                                .color(Color::DARK_TEAL)
+                        })
+                        .components(|c| {
+                            c.create_action_row(|a| {
+                                a.create_button(|b| {
+                                    b.label("Edit Title")
+                                        .style(ButtonStyle::Primary)
+                                        .custom_id("edit_title")
+                                })
+                                .create_button(|b| {
+                                    b.label("Edit Description")
+                                        .style(ButtonStyle::Primary)
+                                        .custom_id("edit_desc")
+                                })
+                                .create_button(|b| {
+                                    b.label("Confirm")
+                                        .style(ButtonStyle::Success)
+                                        .custom_id("send")
+                                })
+                                .create_button(|b| {
+                                    b.label("Cancel")
+                                        .style(ButtonStyle::Danger)
+                                        .custom_id("cancel")
+                                })
+                            })
+                        })
+                    })
+            })
+            .await?;
     }
     Ok(())
 }
